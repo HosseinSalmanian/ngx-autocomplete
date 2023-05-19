e651e3b5fe322aa2541686217bbcc44171ce241c
Hossein Salmanian <Salmanian.Hossein@gmail.com>
2023-05-19 17:03:18 +0330

    set story default argument

--- projects/angular-ngx-autocomplete/src/stories/autocomplete.stories.ts
+++ projects/angular-ngx-autocomplete/src/stories/autocomplete.stories.ts
@@ -5,11 +5,13 @@ import {
 } from 'angular-ngx-autocomplete';
 import { Observable, of, switchMap } from 'rxjs';
 import PERSON_LIST from './data/person-list';
+import { Person } from './models/person.model';
 
-const meta: Meta<AutocompleteComponent<any>> = {
+type Autocomplete = AutocompleteComponent<Person>;
+const meta: Meta<Autocomplete> = {
   component: AutocompleteComponent,
   tags: ['autodocs'],
-  render: (args: AutocompleteComponent<any>) => ({
+  render: (args: Autocomplete) => ({
     moduleMetadata: {
       imports: [AutocompleteModule],
     },
@@ -27,6 +29,30 @@ const meta: Meta<AutocompleteComponent<any>> = {
 };
 
 export default meta;
-type Story = StoryObj<AutocompleteComponent<any>>;
+type Story = StoryObj<Autocomplete>;
 
-export const Demo: Story = {};
+export const Demo: Story = {
+  args: {
+    viewKey: 'email',
+    minimumCharacterForSearch: 2,
+
+    debounceTime:  500,
+  
+    suggestionItemClass: 'custom-class',
+  
+    suggestionContainerHeight: 150, 
+    // suggestionKey!: keyof T;
+    // suggestionMethod!: (item?: T) => string;
+    // idKey: keyof T = 'id' as keyof T;
+    // viewKey!: keyof T;
+    // viewMethod!: (selection?: T) => string;
+    placeholder:'type to search',
+    autocomplete: 'off',
+  
+    noSuggestionText:'no item found.',
+  
+    clearButton: true,
+  
+    fitOverlayWidth:  false
+  },
+};
